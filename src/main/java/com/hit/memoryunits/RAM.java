43845e4f1c78667cc99772cf06e31f1d73382574
Aviad Hemo <aviadhhh@gmail.com>
2017-08-28 12:56:03 +0300

    completed RAM class implementation

--- src/main/java/com/hit/memoryunits/RAM.java
+++ src/main/java/com/hit/memoryunits/RAM.java
@@ -1,50 +1,64 @@
 package com.hit.memoryunits;
 
 
+import java.io.Serializable;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
-public class RAM extends java.lang.Object{
-	int m_InitialCapacity;
+public class RAM implements Serializable{
+	private int initialCapacity;
+	private Map<Long, Page<byte[]>> pages;
+
 	public RAM(int initialCapacity){
-		
+		setInitialCapacity(initialCapacity);
+		pages = new LinkedHashMap<>();
 	}
-	
-	
+
 	public void addPage(Page<byte[]> addPage){
-		
+		pages.put(addPage.getPageId(), addPage);
 	}
 	public void addPages(Page<byte[]>[] addPages){
-		
+		for (Page<byte[]> addPage : addPages) {
+			pages.put(addPage.getPageId(), addPage);
+		}
 	}
 	
 	public int getInitialCapacity(){
-		return m_InitialCapacity;
+		return initialCapacity;
 	}
 	
-	public Page<byte[]> getPage(java.lang.Long pageId){
-		
+	public Page<byte[]> getPage(Long pageId){
+		return pages.get(pageId);
 	}
-	public java.util.Map<java.lang.Long,Page<byte[]>> getPages(){
-		
+	public Map<Long,Page<byte[]>> getPages(){
+		return pages;
 	}
 	
-	public Page<byte[]>[] getPages(java.lang.Long[] pageIds){
-		
+	public Page<byte[]>[] getPages(Long[] pageIds){
+		Page<byte[]>[] returnPages = new Page[pageIds.length];
+		int count = 0
+				;
+		for(int i = 0; i < pageIds.length; i++)
+		{
+			returnPages[i] = pages.get(pageIds[i]);
+		}
+
+		return returnPages;
 	}
 	
 	public void removePage(Page<byte[]> removePage){
-		
+		pages.remove(removePage);
 	}
 	public void removePages(Page<byte[]>[] removePages) {
-		
+		for (Page<byte[]> removePage : removePages) {
+			pages.remove(removePage);
+		}
 	}
 	public void setInitialCapacity(int initialCapacity){
-		m_InitialCapacity=initialCapacity;
+		this.initialCapacity = initialCapacity;
 	}
-	public void setPages(java.util.Map<java.lang.Long,Page<byte[]>> pages){
-		
+
+	public void setPages(Map<Long,Page<byte[]>> pages){
+		this.pages = pages;
 	}
-	
-	
-	
-	
 }
