4be154fd6e61c05fdd265d1d40f6e752aa100ffc
Aviad Hemo <aviadhhh@gmail.com>
2017-08-28 13:15:51 +0300

    implemented HardDisk as Singleton, missing some methods to finish class implementation

--- src/main/java/com/hit/memoryunits/HardDisk.java
+++ src/main/java/com/hit/memoryunits/HardDisk.java
@@ -1,19 +1,45 @@
 package com.hit.memoryunits;
 
-public class HardDisk extends java.lang.Object{
-	Page<byte[]> m_arr;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public final class HardDisk {
+
+	private static HardDisk instance = new HardDisk();
+	private Map<Long,Page<byte[]>> pages;
 	
 	//Need to add try and catch;
-	
-	public Page<byte[]> pageFault(java.lang.Long pageId){
-		return m_arr;
-	}
-	
-	
-	public Page<byte[]> pageReplacement(Page<byte[]> moveToHdPage, java.lang.Long moveToRamId){
-		return m_arr;
-	}
-	
-	
 
+
+	private HardDisk() {
+		this.pages = new LinkedHashMap<>();
+	}
+
+	public Page<byte[]> pageFault(Long pageId) throws FileNotFoundException, IOException{
+
+		if(pages.containsKey(pageId)) {
+			//TODO add missing part, I thing we should write to HD here before return statement
+			return pages.get(pageId);
+		}
+
+		return null;
+	}
+
+	public static HardDisk getInstance() {
+		return instance;
+
+	}
+
+	public Page<byte[]> pageReplacement(Page<byte[]> moveToHdPage, Long moveToRamId) throws FileNotFoundException, IOException{
+		//TODO Complete implementation
+	}
+
+
+	//when clone method is override the singleton is more stronger because the singleton object(HardDisk) cannot be cloned
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		throw new CloneNotSupportedException();
+	}
 }
