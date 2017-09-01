c96def899aed8bb7df5eb3c30e2d405fda1f0df1
Aviad Hemo <aviadhhh@gmail.com>
2017-09-01 19:18:11 +0300

    added javadoc to Page

--- src/main/java/com/hit/memoryunits/Page.java
+++ src/main/java/com/hit/memoryunits/Page.java
@@ -2,20 +2,28 @@ package com.hit.memoryunits;
 
 
 public class Page<T> implements java.io.Serializable {
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
 	
 	private T content;
 	private Long id;
 
 
-	public Page(java.lang.Long id, T content){
+	/**
+	 *
+	 * @param id the id of the page.
+	 * @param content
+	 */
+	public Page(Long id, T content){
 		this.setId(id);
 		this.setContent(content);
 	}
 
+	/**
+	 *
+	 * @param obj the object to compare
+	 * @return indicates if the 2 object are the same instance or have the same id
+	 */
 	@Override
 	public boolean equals(Object obj){
 		if(this == obj) 
