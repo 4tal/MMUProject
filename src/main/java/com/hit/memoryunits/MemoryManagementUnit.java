f08da406211a189ca748be0f7ff02bdda23ae800
Aviad Hemo <aviadhhh@gmail.com>
2017-09-01 18:56:50 +0300

    fix commit- nothing special

--- src/main/java/com/hit/memoryunits/MemoryManagementUnit.java
+++ src/main/java/com/hit/memoryunits/MemoryManagementUnit.java
@@ -3,13 +3,18 @@ package com.hit.memoryunits;
 public class MemoryManagementUnit extends java.lang.Object{
 	//Add 2 members RAM and IAlgoCache
 	//Add capability to read/Write from HD.
-	
-	RAM 
-	
-	public MemoryManagementUnit(int ramCapacity, com.hit.algorithm.IAlgoCache<java.lang.Long,java.lang.Long> algo){
-		
+
+	//RAM
+
+
+	//Check the parameters:
+	public MemoryManagementUnit(int ramCapacity, IAlgoCache<java.lang.Long,java.lang.Long> algo){
+
 	}
-	public Page<byte[]>[] getPages(java.lang.Long[] pageIds){
-		
+
+	/*     Need to complete this method.
+	public Page<byte[]>[] getPages(Long[] pageIds){
 	}
+
+	*/
 }
